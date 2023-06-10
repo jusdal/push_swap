14a45535300f2a166a0bea692ffa11a3ffb12b3e
justin daly <justin.m.daly@gmail.com>
2023-06-10 23:24:20 +0700

    add error messages

--- 0_errors.c
+++ 0_errors.c
@@ -6,7 +6,34 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 23:42:43 by jdaly             #+#    #+#             */
-/*   Updated: 2023/06/08 23:43:39 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/06/10 17:46:53 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    error(char *message)
+{
+    ft_putstr_fd(message, 1);
+    exit(EXIT_FAILURE);
+}
+
+void	free_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+void	free_error(char *message, char **array)
+{
+	free_array(array);
+	error(message);
+}
 
