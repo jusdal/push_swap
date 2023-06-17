15a1722b145b8b93bca37e9a2d7ff2b8a844e65b
justin daly <justin.m.daly@gmail.com>
2023-06-17 18:30:35 +0700

    update header file

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,14 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 15:29:55 by jdaly             #+#    #+#             */
-/*   Updated: 2023/06/15 22:45:21 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/06/16 20:15:26 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <limits.h> //for INT_MIN INT_MAX
 #include <stdio.h> //remove
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -21,12 +22,12 @@ typedef	struct	s_stack_node
     int					value;
     int					current_position;
     int					final_index;
-    int					push_price;
-    bool				above_median;
-    bool				cheapest;
+    // int					push_price;
+    // bool				above_median;
+    // bool				cheapest;
     struct s_stack_node	*target_node;
     struct s_stack_node	*next;
-    struct s_stack_node	*prev;
+    //struct s_stack_node	*prev;
 }	t_stack_node;
 
 /* 0_errors.c */
@@ -34,3 +35,4 @@ void    error(char *message);
 void	free_array(char **array);
 void	free_error(char *message, char **array);
 void    free_ll_error(char *message, char **array, t_stack_node **stack);
+void    free_linkedlist(t_stack_node **stack);
