27406026aebd580be5dd061ebeb046eb4732f7ae
jusdal <justin.m.daly@gmail.com>
2023-06-23 00:30:00 +0700

    update fn src templates

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 15:29:55 by jdaly             #+#    #+#             */
-/*   Updated: 2023/06/22 18:01:30 by justindaly       ###   ########.fr       */
+/*   Updated: 2023/06/23 00:24:52 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,20 @@ typedef struct s_stack_node
 	struct s_stack_node		*next;
 }	t_stack_node;
 
+/* 0_actions1.c */
+void			swap(t_stack_node **a);
+void			push(t_stack_node **dest, t_stack_node **src);
+void			rotate(t_stack_node **stack);
+t_stack_node	*find_second_last_node(t_stack_node *stack);
+void			reverse_rotate(t_stack_node **a);
+
+/* 0_actions2.c */
+void	sa(t_stack_node **a);
+void	sb(t_stack_node **b);
+void	ra(t_stack_node **a);
+void	pa(t_stack_node **a, t_stack_node **b);
+void	pb(t_stack_node **a, t_stack_node **b);
+
 /* 0_errors.c */
 void	error(void);
 void	free_array(char **array);
@@ -37,31 +51,27 @@ void	free_ll_error(char **array, t_stack_node *stack);
 
 /* 0_stack_utils.c */
 int				stack_len(t_stack_node *lst);
-bool			stack_sorted(t_stack_node *stack);
 t_stack_node	*find_last_node(t_stack_node *stack);
-int				find_min(t_stack_node *stack);
+bool			stack_sorted(t_stack_node *stack);
+int				find_next_min(t_stack_node *stack, long curr_min);
 void			assign_index(t_stack_node *a);
 
-/* 0_actions.c */
-void			swap(t_stack_node **a);
-void			push(t_stack_node **dest, t_stack_node **src);
-void			rotate(t_stack_node **stack);
-t_stack_node	*find_second_last_node(t_stack_node *stack);
-void			reverse_rotate(t_stack_node **a);
+/* 1_parse.c */
+void	check_digit(char *str);
+long	ft_atol(char *str, char **array, t_stack_node *a);
+bool	check_dup(t_stack_node *a, int nbr);
+void	append_node(t_stack_node **stack, int n, int pos);
+void	stack_init(t_stack_node **a, char **array);
 
-/* 0_actions.c */
-void	sa(t_stack_node **a);
-void	sb(t_stack_node **b);
-void	ra(t_stack_node **a);
-void	pa(t_stack_node **a, t_stack_node **b);
-void	pb(t_stack_node **a, t_stack_node **b);
-
-
-/* 1_sort_small.c */
+/* 2_sort_small.c */
 void	sort_three(t_stack_node **a, int t, int m, int b);
 void	sort_four(t_stack_node **a, t_stack_node **b);
 void	rotate_to_min(t_stack_node **a);
 void	sort_five(t_stack_node **a, t_stack_node **b);
 void	sort_small(t_stack_node **a, t_stack_node **b, int len);
+
+/* 3_sort_large.c */
+int count_bits(int num);
+void	radix_sort(t_stack_node **a, t_stack_node **b);
 
 #endif
