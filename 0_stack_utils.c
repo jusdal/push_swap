a4cdfa176f6d9624128b5ccc35b7242e656ec5b1
justin daly <justin.m.daly@gmail.com>
2023-06-23 18:24:17 +0700

    fix norm

--- 0_stack_utils.c
+++ 0_stack_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   0_stack_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 21:15:02 by jdaly             #+#    #+#             */
-/*   Updated: 2023/06/22 23:59:33 by justindaly       ###   ########.fr       */
+/*   Updated: 2023/06/23 17:10:49 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,19 +49,13 @@ bool	stack_sorted(t_stack_node *stack)
 
 	temp = stack;
 	min = INT_MIN;
-	//printf("min = %d\n", min);
 	while (temp)
 	{
-		//printf("temp->value = %d \\ min = %d\n", temp->value, min);
 		if (temp->value < min)
-		{
-			//printf("STACK IS NOT SORTED!\n");
 			return (false);
-		}
 		min = temp->value;
 		temp = temp->next;
 	}
-	//printf("STACK IS SORTED!\n");
 	return (true);
 }
 
@@ -81,27 +75,23 @@ int	find_next_min(t_stack_node *stack, long curr_min)
 
 void	assign_index(t_stack_node *a)
 {
-	int	i;
-	int	len;
-	long min;
-	t_stack_node *temp;
+	int				i;
+	int				len;
+	long			min;
+	t_stack_node	*temp;
 
 	i = 1;
 	len = stack_len(a);
-	//printf("len = %d\n", len);
 	temp = a;
 	min = find_next_min(a, LONG_MIN);
 	while (i <= len)
 	{
-
 		while (a)
 		{
-			//printf("min = %ld\n", min);
 			if (a->value == min)
 			{
 				a->index = i;
 				min = find_next_min(temp, min);
-				//printf("index of value %d is %d\n", a->value, a->index);
 				break ;
 			}
 			a = a->next;
